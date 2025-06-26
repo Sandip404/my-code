74034a2648ecfa24e349bb1d7a434fc81a015562
sandip <sandip@debian>
2025-06-26 23:22:00 +0530

    Initial commit from Debian

---
+++ cpp/1.intro into cpp, structure, input & output/o/3.inputs-and-outputs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b; // Declare two integer variables
+
+    cout << "Enter a number: "; // Prompt user for input
+    cin >> a; // Read first number from user
+
+    cout << "You entered: " << a <<  endl; // Output the numbers
+
+    return 0; // End of program
+}
