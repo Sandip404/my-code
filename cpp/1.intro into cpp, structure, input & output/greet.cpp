ca7f4cc600a013c801fdc2c52a2a01313f0b5c2a
Sandip404 <sandipbag861@gmail.com>
2025-06-29 12:41:15 +0530

    from linux

---
+++ cpp/1.intro into cpp, structure, input & output/greet.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string name;
+    cout << "\nenter your name : ";
+    cin >> name;
+    cout << "\nHello, " << name << "!" << endl;
+    return 0;
+}
---
+++ cpp/1.intro into cpp, structure, input & output/number.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "\nenter two numbers : ";
+    cin >> a >> b;
+    cout << "\nyou enter : " << a << " \nand then : " << b   << endl;
+    return 0;
+}
