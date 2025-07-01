521ea991167df1c8aa399ffbccbfe0268792aa0e
Sandip404 <sandipbag861@gmail.com>
2025-07-01 23:25:03 +0530

    from home

---
+++ c/01.intro into c, compilation/comment.c
@@ -0,0 +1,6 @@
+
+
+//this is a single line comment
+
+/* this is a multi-line comment
+   that spans multiple lines */
---
+++ c/01.intro into c, compilation/number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("your number is %d\n", n);
+
+    return 0;
+}
